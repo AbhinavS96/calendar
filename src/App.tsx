d69554b4b95c450cfaf41519cba16f15e7800a87
Abhinav <abhinavsidharth@gmail.com>
2023-04-04 12:54:59 +0200

    set up states. need to move to context

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
-import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import { Calendar, Views, dayjsLocalizer, View } from "react-big-calendar";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
@@ -26,31 +26,45 @@ function App() {
   const [date, setDate] = useState(
     dayjs("Tue Apr 04 2023 21:54:06 GMT+0200 (Central European Summer Time)")
   );
+  const [view, setView] = useState<View>(Views.MONTH);
   //console.log(date.toDate());
   return (
     <div className="App">
-      <Calendar
-        localizer={localizer}
-        events={event}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        onSelectEvent={() => console.log("event")}
-        onSelectSlot={() => console.log("slot")}
-        date={date.toDate()}
-        onNavigate={(a) => {
-          setDate(dayjs(a));
-        }}
-        selectable
-      />
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar
-          value={date}
-          onChange={(date) => {
-            if (date) setDate(date);
+      <div className="big-container">
+        <Calendar
+          localizer={localizer}
+          events={event}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+          onSelectEvent={() => console.log("event")}
+          onSelectSlot={() => console.log("slot")}
+          date={date.toDate()}
+          onNavigate={(a) => {
+            setDate(dayjs(a));
+          }}
+          selectable
+          views={[Views.DAY, Views.WEEK, Views.MONTH]}
+          view={view}
+          onView={(a) => {
+            console.log(a);
+            setView(a);
           }}
         />
-      </LocalizationProvider>
+      </div>
+      <div className="small-container">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateCalendar
+            value={date}
+            onChange={(date) => {
+              if (date) {
+                setDate(date);
+                setView(Views.DAY);
+              }
+            }}
+          />
+        </LocalizationProvider>
+      </div>
     </div>
   );
 }
